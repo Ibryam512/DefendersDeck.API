0fe7616d0de29fd61f364e5ecaeb161699eab9a5
Ibryam512 <bamko2003@gmail.com>
2025-01-15 19:51:06 +0200

    Add image urls to cards

--- DefendersDeck.DataAccess/Migrations/20250115175019_AddImageUrls.Designer.cs
+++ DefendersDeck.DataAccess/Migrations/20250115175019_AddImageUrls.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using DefendersDeck.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace DefendersDeck.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250115175019_AddImageUrls")]
+    partial class AddImageUrls
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DefendersDeck.DataAccess/Migrations/20250115175019_AddImageUrls.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DefendersDeck.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageUrls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreationDate", "ImageUrl" },
+                values: new object[] { new DateTime(2025, 1, 14, 22, 0, 0, 0, DateTimeKind.Utc), "fire-arrows.png" });
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreationDate",
+                value: new DateTime(2025, 1, 14, 22, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreationDate",
+                value: new DateTime(2025, 1, 14, 22, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreationDate",
+                value: new DateTime(2025, 1, 14, 22, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreationDate",
+                value: new DateTime(2025, 1, 14, 22, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreationDate",
+                value: new DateTime(2025, 1, 14, 22, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreationDate",
+                value: new DateTime(2025, 1, 14, 22, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "CreationDate", "ImageUrl" },
+                values: new object[] { new DateTime(2025, 1, 14, 22, 0, 0, 0, DateTimeKind.Utc), "health-potion.png" });
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "CreationDate", "ImageUrl" },
+                values: new object[] { new DateTime(2025, 1, 14, 22, 0, 0, 0, DateTimeKind.Utc), "shadow-army.png" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreationDate", "ImageUrl" },
+                values: new object[] { new DateTime(2025, 1, 8, 22, 0, 0, 0, DateTimeKind.Utc), "" });
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreationDate",
+                value: new DateTime(2025, 1, 8, 22, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreationDate",
+                value: new DateTime(2025, 1, 8, 22, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreationDate",
+                value: new DateTime(2025, 1, 8, 22, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreationDate",
+                value: new DateTime(2025, 1, 8, 22, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "CreationDate",
+                value: new DateTime(2025, 1, 8, 22, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "CreationDate",
+                value: new DateTime(2025, 1, 8, 22, 0, 0, 0, DateTimeKind.Utc));
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "CreationDate", "ImageUrl" },
+                values: new object[] { new DateTime(2025, 1, 8, 22, 0, 0, 0, DateTimeKind.Utc), "" });
+
+            migrationBuilder.UpdateData(
+                table: "Cards",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "CreationDate", "ImageUrl" },
+                values: new object[] { new DateTime(2025, 1, 8, 22, 0, 0, 0, DateTimeKind.Utc), "" });
+        }
+    }
+}
